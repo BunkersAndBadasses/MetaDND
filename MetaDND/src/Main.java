9b6b09e161e87b4f1f4a5642e6ec7032bcc2a32f
Helseth <Helseth@Curt-Desktop>
2015-03-18 12:51:20 -0500

    Some test code

--- MetaDND/src/Main.java
+++ MetaDND/src/Main.java
@@ -1,9 +1,17 @@
+import org.eclipse.swt.widgets.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setText("Testing box");
+		shell.open();
+		while(!shell.isDisposed()){
+			if(!display.readAndDispatch())
+				display.sleep();
+		}
+		display.dispose();
 	}
 
 }
