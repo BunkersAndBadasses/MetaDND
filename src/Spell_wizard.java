fe94bcda772a22e4ec62353286efddfc9336bec9
Innocentius <lzhao49@wisc.edu>
2015-03-19 21:46:08 -0500

    ...

--- src/Spell_wizard.java
+++ src/Spell_wizard.java
@@ -1,13 +1,4 @@
-import org.eclipse.swt.widgets.Display;
 
-/**
- * The class that handle Spell wizard interface, input and export.
- * @author Innocentius Shellingford
- *
- */
 public class Spell_wizard {
-	public Spell_wizard(Display display)
-	{
-		
-	}
+
 }
