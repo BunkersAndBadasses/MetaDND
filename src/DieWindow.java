0cc57bde89e02d18930969b96f5513436f218886
Innocentius <lzhao49@wisc.edu>
2015-03-19 20:46:08 -0500

    One step

--- src/DieWindow.java
+++ src/DieWindow.java
@@ -1,7 +1,9 @@
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.*;
 
 /*
  * The class for the Die Window.
@@ -9,11 +11,22 @@ import org.eclipse.swt.widgets.Shell;
  */
 public class DieWindow {
 	private Shell shell;
+	private Label label;
+	private Text text;
+	private Button ok;
 	public DieWindow(Display display)
 	{
 		shell = new Shell(display);
 		shell.setText("Die Roller");
+		label = new Label(shell, SWT.NONE);
+		label.setText("TEXT");
+		text = new Text (shell, SWT.BORDER);
+		text.setLayoutData (new RowData (100, SWT.DEFAULT));
+		ok = new Button (shell, SWT.PUSH);
+		ok.setText ("OK");
+		shell.setDefaultButton (ok);
+		shell.setLayout (new RowLayout ());
+		shell.pack ();
 		shell.open();
-		shell.setLayout(new FillLayout());
 	}
 }
