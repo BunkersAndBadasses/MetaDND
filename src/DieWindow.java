4241bfc596b2bd04224ae33d0c272e74aafc658f
Innocentius <lzhao49@wisc.edu>
2015-03-19 20:55:13 -0500

    Config die

--- src/DieWindow.java
+++ src/DieWindow.java
@@ -1,4 +1,6 @@
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.RowData;
@@ -13,18 +15,24 @@ public class DieWindow {
 	private Shell shell;
 	private Label label;
 	private Text text;
-	private Button ok;
+	private Button roll_d20;
 	public DieWindow(Display display)
 	{
 		shell = new Shell(display);
 		shell.setText("Die Roller");
 		label = new Label(shell, SWT.NONE);
-		label.setText("TEXT");
+		label.setText("D20");
 		text = new Text (shell, SWT.BORDER);
 		text.setLayoutData (new RowData (100, SWT.DEFAULT));
-		ok = new Button (shell, SWT.PUSH);
-		ok.setText ("OK");
-		shell.setDefaultButton (ok);
+		roll_d20 = new Button (shell, SWT.PUSH);
+		roll_d20.setText ("Roll");
+		roll_d20.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				//add die function
+			}	
+		});
+		shell.setDefaultButton (roll_d20);
 		shell.setLayout (new RowLayout ());
 		shell.pack ();
 		shell.open();
