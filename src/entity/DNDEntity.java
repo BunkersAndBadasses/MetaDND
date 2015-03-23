6a072f0de380ffac94603c68dabc2ac1e858efb2
Helseth <Helseth@Curt-Desktop>
2015-03-23 15:34:51 -0500

    Created abstract Entity class fo searchables

--- src/entity/DNDEntity.java
+++ src/entity/DNDEntity.java
@@ -1,5 +1,14 @@
 package entity;
-
-public class DNDEntity {
+/*
+ * Generic entity class, extend this when creating searchable entities
+ */
+public abstract class DNDEntity {
+	
+	String name;
+	String description;
+	//TODO Replace void with actual window object
+	abstract String getName();
+	abstract String getDescription();
+	abstract void toTooltipWindow();
 
 }
