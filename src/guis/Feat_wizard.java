4c157dd9dadf98191a3458803322e58617305df7
Innocentius <lzhao49@wisc.edu>
2015-04-02 11:50:47 -0500

    Remove old material

--- src/guis/Feat_wizard.java
+++ src/guis/Feat_wizard.java
@@ -1,9 +1,13 @@
+package guis;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+
+import entity.FeatEntity;
 
 /**
  * The class that handle Feat wizard interface, input and export.
@@ -18,21 +22,24 @@ public class Feat_wizard
 	private int pagenum;
 	private static final int WIDTH = 700;
 	private static final int HEIGHT = 500;//copy from character wizard, see for change
-	private Feat newfeat;
+	private FeatEntity newfeat;
+	String featname;
+	String featprereq;
+	String featnormal;
+	String featspecial;
+	String featfighter;
+	String featscript;
 	public Feat_wizard(Display d)
 	{
 		display = d;
 		shell = new Shell(d);
 		shell.setText("Feat Wizard");
 		shell.setSize(WIDTH,HEIGHT);
-		newfeat = new Feat();
 	}
 	public void run()
 	{
 		center(shell);
 
-		setBackgroundColor();
-		
         shell.open();
 
         while (!shell.isDisposed()) {
@@ -58,20 +65,28 @@ public class Feat_wizard
         shell.setBounds(nLeft, nTop, p.x, p.y);
     }
 	/**
-	 * From Character Wizard.
+	 * To create a feat, we need user input from the following:
+	 * NAME
+	 * PREREQUISITE
+	 * NORMAL 
+	 * SPECIAL
+	 * BENEFIT
+	 * FIGHTERBONUS
+	 * DESCRIPTION
+	 * 
+	 * We get input of name, prerequisite on the same page.
+	 * Then second on normal, special, benefit, fighter bonus.
+	 * Description at the third.
+	 * We get the inputs, put them into Strings, hash map it, then create a new feat... BUT where should I put it?
+	 * TODO verify the storing method
 	 */
-	private void setBackgroundColor() 
-	{
-	    Color red = display.getSystemColor(SWT.COLOR_RED);
-	    
-	    //shell.setLayout(new FillLayout());
-
-	    shell.setBackground(red);
-	}
 	private void createPageContent() 
 	{
 		
 	}
+	/**
+	 * simple getter
+	 */
 	public Shell getshell()
 	{
 		return shell;
