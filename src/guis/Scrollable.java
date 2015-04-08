705b1c63d923964295a9abcc441c5e1deb2d3f45
Sarah <sszczerbinsk@wisc.edu>
2015-04-07 20:19:53 -0500

    updated scrollable to single column grid layout

--- src/guis/Scrollable.java
+++ src/guis/Scrollable.java
@@ -2,6 +2,7 @@ package guis;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 
 public class Scrollable {
@@ -17,7 +18,10 @@ public class Scrollable {
 		sc.setMinSize(minWidth, minHeight);
 		c = new Composite(sc, SWT.NONE);
 		sc.setContent(c);
-		c.setSize(c.computeSize(SWT.DEFAULT, SWT.DEFAULT));		
+		c.setSize(c.computeSize(SWT.DEFAULT, SWT.DEFAULT));	
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 1;
+		c.setLayout(layout);
 	}
 	
 }
