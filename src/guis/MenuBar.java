2bb28eb1b7458c28fdf96cfe144ee5eb088dd92b
Sarah <sszczerbinsk@wisc.edu>
2015-04-09 18:30:40 -0500

    added character wiz to menu bar

--- src/guis/MenuBar.java
+++ src/guis/MenuBar.java
@@ -168,7 +168,7 @@ public class MenuBar {
         charGenItem.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
-                //TODO
+        		new CharacterWizard(display);
             }
         });
         
