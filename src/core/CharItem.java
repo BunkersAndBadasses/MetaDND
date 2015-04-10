ff904a3d8c22ecc1cd6bb1f4192175897fc134e7
Sarah <sszczerbinsk@wisc.edu>
2015-04-09 20:09:19 -0500

    added char item

---
+++ src/core/CharItem.java
@@ -0,0 +1,37 @@
+package core;
+
+import entity.ItemEntity;
+
+public class CharItem {
+	private ItemEntity item;
+	private int count;
+	
+	public CharItem(ItemEntity i) {
+		item = i;
+		count = 1;
+	}
+	
+	public void incCount() {
+		count++;
+	}
+	
+	public boolean decCount() {
+		if (count == 0) 
+			return false;
+		count--;
+		return true;
+	}
+	
+	public String getName() {
+		return item.getName();
+	}
+	
+	public ItemEntity getItem() {
+		return item;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+}
