02c09193987846e3a7fb2f9e417a3204f81eabb7
Innocentius <lzhao49@wisc.edu>
2015-04-16 21:30:11 -0500

    Update RNG comment

--- src/core/RNG.java
+++ src/core/RNG.java
@@ -10,6 +10,13 @@ public class RNG {
 		this.m_random = new Random(System.currentTimeMillis());
 	}
 
+	/**
+	 * A random generater that generate an integer given boundary of min and max.
+	 * WILL GENERATE A NEGATIVE NUMBER IF MAX < MIN!
+	 * @param min
+	 * @param max
+	 * @return
+	 */
 	public int GetRandomInteger(int min, int max){
 		if (min == max) {
 			return this.m_random.nextInt(max + 1) + min;
