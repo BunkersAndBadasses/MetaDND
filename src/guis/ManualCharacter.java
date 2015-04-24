3746451951b7af99fcba6d900a672f58808420c3
Ryan <rranney255@gmail.com>
2015-04-23 22:48:21 -0500

    Made a template for the manual page

---
+++ src/guis/ManualCharacter.java
@@ -0,0 +1,34 @@
+package guis;
+
+import core.character;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Device;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
+
+public class ManualCharacter {
+	
+	private Composite manual;
+	
+	public ManualCharacter(){
+		
+		
+		createPageContent();
+	}
+
+	private void createPageContent() {
+		
+		
+	}
+	
+}
