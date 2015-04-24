717d1fbfc07f74d1ecd8329cd5f32f802544aa2d
Ryan <rranney255@gmail.com>
2015-04-23 23:01:41 -0500

    manual skeleton update

--- src/guis/ManualCharacter.java
+++ src/guis/ManualCharacter.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.custom.StackLayout;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Device;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
@@ -18,15 +20,21 @@ import org.eclipse.swt.widgets.Text;
 
 public class ManualCharacter {
 	
-	private Composite manual;
+	private ScrolledComposite manual;
 	
-	public ManualCharacter(){
-		
+	public ManualCharacter(Composite page){
+		manual = new ScrolledComposite(page, SWT.V_SCROLL);
 		
 		createPageContent();
 	}
 
 	private void createPageContent() {
+		
+		GridLayout layout = new GridLayout();
+		layout.makeColumnsEqualWidth = false;
+		layout.horizontalSpacing = 3;
+		layout.numColumns = 3;
+		manual.setLayout(layout);
 		
 		
 	}
